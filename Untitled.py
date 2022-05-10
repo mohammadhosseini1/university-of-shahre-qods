98271a9813c7363d5f2cfed922d1632f279c174f
mohammadhosseini1 <104566410+mohammadhosseini1@users.noreply.github.com>
2022-05-11 00:49:46 +0430

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def all_unique(1st):
+    x=[1,2,2,32,3,4,5]
+    y=[1,2,3,4,5]
+    all_unique(x)#false
+        all_unique(x)#True
+        
+
+    
+
+
+# In[3]:
+
+
+def all_unique(1st):
+    return len(1st)==len(set(1St))
+x=[44,74,77,55,66,88,22]
+y=[10,17,18,13]
+ all_unique(x)#false
+     all_unique(x)#True
+        
+
+
+# In[4]:
+
+
+from collections import counter
+def angram (first ,second)==counter(second)
+anagram("abcd","3acdb")#True
+
+
+# In[ ]:
+
+
+
+
